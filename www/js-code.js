51439664dca6e365236b38687d9e31858d4b2317
Fernando Cagua <efcaguab@gmail.com>
2018-04-04 21:14:06 +1200

    Add carrousel for all variables and create columns in js

--- www/js-code.js
+++ www/js-code.js
@@ -9,7 +9,7 @@ function make_option(factor, level, emoji_char, option_title, option_text){
   link.id = button_id.concat(factor, "_", level)
   // Make image
   var emoji_title = document.createElement("h3")
-  emoji_title.classList.add("display-7")
+  emoji_title.classList.add("display-1")
   emoji_title.innerText = emoji_char
   // Add image to link
   link.appendChild(emoji_title)
@@ -36,3 +36,41 @@ function make_option_column(factor, level, img_src, option_title, option_text){
   column_element.appendChild(make_option(factor, level, img_src, option_title, option_text))
   return column_element
 }
+
+document.getElementById("genero_row").appendChild(make_option_column("genero", "femenino", "👩", "Mujer", undefined))
+document.getElementById("genero_row").appendChild(make_option_column("genero", "masculino", "👨", "Hombre", undefined))
+
+document.getElementById("zona_row").appendChild(make_option_column("zona", "urbano", "👩‍💼", "Urbano", undefined))
+document.getElementById("zona_row").appendChild(make_option_column("zona", "rural", "👩‍🌾", "Rural", undefined))
+
+document.getElementById("estrato_row").appendChild(make_option_column("estrato", "bajo", "🏠", "Bajo", "Estrato 1 y 2"))
+document.getElementById("estrato_row").appendChild(make_option_column("estrato", "medio", "🏢", "Medio", "Estrato 3"))
+document.getElementById("estrato_row").appendChild(make_option_column("estrato", "alto", "🏰", "Alto", "Estrato 4, 5 y 6"))
+
+document.getElementById("edad_row").appendChild(make_option_column("edad", "18.24", "👶", "18 - 24", undefined))
+document.getElementById("edad_row").appendChild(make_option_column("edad", "25.34", "👦", "25 - 34", undefined))
+document.getElementById("edad_row").appendChild(make_option_column("edad", "35.44", "👨", "35 - 44", undefined))
+document.getElementById("edad_row").appendChild(make_option_column("edad", "45.54", "🧓", "45 - 54", undefined))
+document.getElementById("edad_row").appendChild(make_option_column("edad", "55", "👴", "> 55", undefined))
+
+document.getElementById("region_row").appendChild(make_option_column("region", "bogota", "🏙", "Bogota", undefined))
+document.getElementById("region_row").appendChild(make_option_column("region", "norte", "🏖", "Costa", "Atlántico, Bolívar, Cesar, Cordoba, Guajira, Magdalena y Sucre"))
+document.getElementById("region_row").appendChild(make_option_column("region", "centrooriental", "⛰", "Centro-oriente", "Boyacá, Caquetá, Cundinamarca, Huila, Meta, Tolima, Santander y Norte de Santander"))
+document.getElementById("region_row").appendChild(make_option_column("region", "cafetera", "☕", "Cafetera", "Antioquia, Caldas, Quindío y Risaralda"))
+document.getElementById("region_row").appendChild(make_option_column("region", "suroccidental", "🌳", "Sur-occidente", "Valle, Cauca, Choco y Nariño"))
+
+
+document.getElementById("consulta_row").appendChild(make_option_column("consulta", "gran", "🐖", "Si", "En la consulta de Duque, Ramírez y Ordoñez"))
+document.getElementById("consulta_row").appendChild(make_option_column("consulta", "inclusion", "🐀", "Si", "En la consulta de Petro y Caicedo"))
+document.getElementById("consulta_row").appendChild(make_option_column("consulta", "no.consulta", "🐇", "Si", "Pero no en ninguna consulta"))
+document.getElementById("consulta_row").appendChild(make_option_column("consulta", "no", "🚫", "No", undefined))
+
+document.getElementById("partido_row").appendChild(make_option_column("partido", "no", "💔", "Ningún partido", undefined))
+document.getElementById("partido_row").appendChild(make_option_column("partido", "verde", "💚", "Partido verde", undefined))
+document.getElementById("partido_row").appendChild(make_option_column("partido", "polo", "💛", "Polo", undefined))
+document.getElementById("partido_row").appendChild(make_option_column("partido", "conservador", "💙", "Partido Conservador", undefined))
+document.getElementById("partido_row").appendChild(make_option_column("partido", "decencia", "🧡", "Coalicion de la Decencia", undefined))
+document.getElementById("partido_row").appendChild(make_option_column("partido", "liberal", "❤", "Partido liberal", undefined))
+document.getElementById("partido_row").appendChild(make_option_column("partido", "u", "💜", "Partido de la U", undefined))
+document.getElementById("partido_row").appendChild(make_option_column("partido", "centro.democratico", "🖤", "Centro Democrático", undefined))
+document.getElementById("partido_row").appendChild(make_option_column("partido", "cambio.radical", "💜", "Cambio Radical", undefined))
