90b60c9a82cf913b25774e19e5ef15f0b793ef69
Fernando Cagua <efcaguab@gmail.com>
2018-04-05 14:59:30 +1200

    Make links go to nowhere

--- www/js-code.js
+++ www/js-code.js
@@ -5,7 +5,7 @@ function make_option(factor, level, img_src, option_title, option_text){
   link.classList.add("action-button")
   link.classList.add("shiny-bound-input")
   link.classList.add("text-dark")
-  link.setAttribute("href", "#")
+  link.setAttribute("href", "#!")
   var button_id = "i_"
   link.id = button_id.concat(factor, "_", level)
   // Make image
