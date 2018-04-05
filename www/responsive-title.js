02f993404c13c96aaaf150c9751d247e46017031
Fernando Cagua <efcaguab@gmail.com>
2018-04-06 09:19:14 +1200

    Make title fancy and respond to device size

---
+++ www/responsive-title.js
@@ -0,0 +1,13 @@
+function resize_font(target_element_id, reference_element_id) {
+  var el = document.getElementById(target_element_id);
+  var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
+  var fontSize = parseFloat(style);
+  var factor = document.getElementById(reference_element_id).offsetWidth / el.offsetWidth
+  el.style.fontSize = (factor * fontSize) + "px"
+}
+
+resize_font("title1", "subtitle")
+resize_font("title2", "subtitle")
+
+$(window).resize(function() { resize_font("title2", "subtitle") });
+
