4a78cd2a5255178a7f04e3b5896ddb0c90276f4d
Fernando Cagua <efcaguab@gmail.com>
2018-04-08 20:16:51 +1200

    Aesthetic adjustments to responsive title and subtitle

--- www/responsive-title.js
+++ www/responsive-title.js
@@ -3,11 +3,11 @@ function resize_font(target_element_id, reference_element_id) {
   var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
   var fontSize = parseFloat(style);
   var factor = document.getElementById(reference_element_id).offsetWidth / el.offsetWidth
-  el.style.fontSize = (factor * fontSize) + "px"
+  el.style.fontSize = (factor * fontSize * 0.92) + "px"
 }
 
-resize_font("title1", "subtitle")
-resize_font("title2", "subtitle")
+resize_font("title1", "introduction")
+resize_font("title2", "introduction")
 
 // $(window).resize(function() { resize_font("title2", "subtitle") });
 
